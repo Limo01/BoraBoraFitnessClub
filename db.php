7890cb3aad97dd9d48e7cfbf36fdac70d7b7e2fc
Alberto Lazari <alberto.lazari@gmail.com>
2022-01-08 18:37:29 +0100

    Merge

--- db.php
+++ db.php
@@ -36,42 +36,9 @@
             }
         }
 
-<<<<<<< HEAD
         public function doWriteQuery($query) {
             $queryResult = mysqli_query($this->connection, $query) or die("Errore in doReadQuery(): " . mysqli_error($this->connection));
             return mysqli_affected_rows($this->connection) > 0;
-=======
-        public function isUsernameCorrect($submitted) {
-            //TODO: anti injection
-            $query = "SELECT username FROM cliente WHERE username = '".$submitted."'";
-            $queryResult = mysqli_query($this->connection, $query) or die("Errore in isUsernameCorrect(): " . mysqli_error($this->connection));
-    
-            if(mysqli_num_rows($queryResult) != 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        public function isPasswordCorrect($name, $password) {
-            //TODO: anti injection
-            $query = "SELECT password FROM cliente WHERE username = '".$name."'";
-            $queryResult = mysqli_query($this->connection, $query) or die("Errore in isPasswordCorrect(): " . mysqli_error($this->connection));
-    
-            $row = mysqli_fetch_row($queryResult);
-
-            if(isset($row[0]) && $row[0] == $password) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-
-        public function doWriteQuery($query) {
-            $queryResult = msqli_query($this->connection, $query) or die("Errore in doReadQuery(): " . msqli_error($this->connection));
-            return msqli_affected_rows($this->connection) > 0;
->>>>>>> cdb62e457d19602a1039dbc9cbfad83d522a7b46
         }
     }
 ?>
