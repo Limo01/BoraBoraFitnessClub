30ee55330105150ac2dd568f8b2af1595af64fbc
Limo01 <marcoandrea0145@gmail.com>
2022-01-09 16:09:16 +0100

    Rinominato file pagina area-personale

--- area-personale.php
+++ area-personale.php
@@ -10,17 +10,76 @@
 	$user = "user";
 
 	if ($connessioneOK) {
-		$result = $connessione->doReadQuery("SELECT * FROM utente WHERE username = '". $user . "'");
+		$result = $connessione->doReadQuery("SELECT * FROM utente WHERE username=?", "s", $user);
+		$datiPersonali = $result[0];
+
+		$ultimoIngresso = $connessione->doReadQuery("SELECT dataora_entrata FROM accesso WHERE username_utente=? order by dataora_entrata DESC limit 1", "s", $user)[0];
+
+		$schedeSeguite =  $connessione->doReadQuery("SELECT id, nome, descrizione FROM utente_allenamento JOIN allenamento ON (utente_allenamento.id_allenamento=allenamento.id) WHERE utente_allenamento.username_utente=? AND utente_allenamento.username_utente!=allenamento.username_utente", "s", $user);
+
+		$schedeCreate = $connessione->doReadQuery("SELECT id, nome, descrizione FROM allenamento WHERE allenamento.username_utente=?", "s", $user);
+
 		$connessione->closeConnection();
 
-		$datiPersonali = $result[0];
-		
+		//Informazioni personali
 		$paginaHTML = str_replace("<nome />", $datiPersonali["nome"], $paginaHTML);
 		$paginaHTML = str_replace("<cognome />", $datiPersonali["cognome"], $paginaHTML);
 		$paginaHTML = str_replace("<email />", $datiPersonali["email"], $paginaHTML);
 		$paginaHTML = str_replace("<numero_telefono />", $datiPersonali["numero_telefono"], $paginaHTML);
 		$paginaHTML = str_replace("<data_nascita />", $datiPersonali["data_nascita"], $paginaHTML);
 		$paginaHTML = str_replace("<badge />", $datiPersonali["badge"], $paginaHTML);
+
+		//Riempimento dati abbonamento
+		if($datiPersonali["nome_abbonamento"] == null){
+			$paginaHTML = str_replace("<abbonamento />", "Nessuno", $paginaHTML);
+			$paginaHTML = str_replace("<scadenza_abbonamento />", "Nessuna", $paginaHTML);
+		}
+		else{
+			$paginaHTML = str_replace("<abbonamento />", $datiPersonali["nome_abbonamento"], $paginaHTML);
+			$paginaHTML = str_replace("<scadenza_abbonamento />", $datiPersonali["data_fine"], $paginaHTML);
+		}
+		$paginaHTML= str_replace("<entrate />", $datiPersonali["entrate"], $paginaHTML);
+
+		//Riempimento dati ultimo ingresso
+		if($ultimoIngresso == null){
+			$paginaHTML = str_replace("<data_ingresso />", "Nessuna", $paginaHTML);
+			$paginaHTML = str_replace("<ora_ingresso />", "Nessuna", $paginaHTML);
+		}
+		else{
+			$ultimoIngresso = explode(" ", $ultimoIngresso["dataora_entrata"]);
+
+			$paginaHTML = str_replace("<data_ingresso />", $ultimoIngresso[0], $paginaHTML);
+			$paginaHTML = str_replace("<ora_ingresso />", $ultimoIngresso[1], $paginaHTML);
+		}
+
+		//Riempimento dati schede seguite
+		if($schedeSeguite == null){
+			$paginaHTML = str_replace("<allenamenti_seguiti />", "<p>Nessuna scheda allenamento seguita</p>", $paginaHTML);
+		}
+		else{
+			$output= "";
+			foreach($schedeSeguite as $allenamento){
+				$output= $output . "<a href=\"scheda-allenamento.php?pagina=" . $allenamento["id"] . "\"><article>";
+				$output= $output . "<h3>" . $allenamento["nome"] . "</h3>";
+				$output= $output . "<p>" . $allenamento["descrizione"] . "</p></article></a>";	
+			}
+			$paginaHTML = str_replace("<allenamenti_seguiti />", $output, $paginaHTML);
+		}
+
+		//Riempimento dati schede create
+		if($schedeCreate == null){
+			$paginaHTML = str_replace("<allenamenti_creati />", "<p>Nessun allenamento creato</p>", $paginaHTML);
+		}
+		else{
+			$output= "";
+			foreach($schedeCreate as $allenamento){
+				$output= $output . "<a href=\"scheda-allenamento.php?pagina=" . $allenamento["id"] . "\"><article>";
+				$output= $output . "<h3>" . $allenamento["nome"] . "</h3>";
+				$output= $output . "<p>" . $allenamento["descrizione"] . "</p></article></a>";	
+			}
+			$paginaHTML = str_replace("<allenamenti_creati />", $output, $paginaHTML);
+		}
+
 	} else {
 		$listaClienti = "<p>I sistemi sono al momento non disponibili, riprova più tardi!</p>";
 	}
