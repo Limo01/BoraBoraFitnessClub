f00b468debc2fbb41cf32aa5def40d692190ac29
Limo01 <marcoandrea0145@gmail.com>
2022-01-12 21:29:23 +0100

    Sistemato contatore persone nella home. Ora fa una chiamata AJAX ogni 8 secondi ad un file PHP. Molto più realistico

---
+++ script-home.js
@@ -0,0 +1,22 @@
+function initCounter(){
+	setInterval(function () {
+		var xhttp = new XMLHttpRequest();
+		var n= parseInt(document.getElementById("contatorePersone").innerHTML);
+
+		xhttp.onreadystatechange = function() {
+		   	if (this.readyState == 4 && this.status == 200) {
+		    	document.getElementById("contatorePersone").innerHTML = this.responseText;
+		    }
+		};
+		
+		xhttp.open("GET", "number_generator.php?n="+n, true);
+		xhttp.send();
+	}, 8000);
+}
+
+var handlerPrec= window.onload;
+
+window.onload= function(){
+	handlerPrec();
+	initCounter();
+}
