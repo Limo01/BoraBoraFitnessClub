b2dab7ef58075ccea29e22a142cbe26053a8391d
Francesco Protopapa <francesco.protopapa22@gmail.com>
2022-01-15 20:54:54 +0100

    aggiungi e rimuovi esercizio funzionano

--- generatoreScheda.php
+++ generatoreScheda.php
@@ -2,34 +2,99 @@
     require_once "db.php";
     use DB\DBAccess;
     session_start();
+    if (isset($_POST['creaSchedaSubmit'])) {
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
 
-    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true){
+            $nome = $_POST["nomeScheda"];
+            $descrizione = $_POST["descrizioneScheda"];
+            if (isset($_POST["usernameScheda"])) {
+                $username = $_POST["usernameScheda"];
+            } else {
+                $username = $_SESSION["username"];
+            }
+            $data = date("Y-m-d");
+            $trainer = $_POST["allenatoreScheda"];
 
-        $nome = $_POST["nomeScheda"];
-        $descrizione = $_POST["descrizioneScheda"];
-        if(isset($_POST["usernameScheda"])){
-            $username = $_POST["usernameScheda"];
+            $connessione = new DBAccess();
+            $connessioneOK = $connessione->openDBConnection();
+
+            if ($connessioneOK) {
+                $connessione->doWriteQuery(
+                    "INSERT INTO allenamento(nome, descrizione, username_utente, data_creazione, id_personal_trainer)
+            VALUES(?,?,?,?,?)",
+                    "sssss",
+                    $nome,
+                    $descrizione,
+                    $username,
+                    $data,
+                    $trainer
+                );
+                $idSchedaQuery = $connessione->doReadQuery("SELECT max(id) as maxID FROM allenamento WHERE username_utente = ?", "s", $username);
+                $idScheda = $idSchedaQuery[0]["maxID"];
+                $connessione->closeConnection();
+                header("location: modificaAllenamento.php?id=" . $idScheda);
+            } else {
+                $out = "<p>I sistemi sono al momento non disponibili, riprova più tardi!</p>";
+            }
         } else {
-            $username = $_SESSION["username"];
+            header("location: autenticaione.php");
         }
-        $data = date("Y-m-d");
-        $trainer = $_POST["allenatoreScheda"];
-        
-        $connessione = new DBAccess();
-        $connessioneOK = $connessione->openDBConnection();
-        
-        if ($connessioneOK) {
-            $connessione->doWriteQuery("INSERT INTO allenamento(nome, descrizione, username_utente, data_creazione, id_personal_trainer)
-        VALUES(?,?,?,?,?)", "sssss",
-        $nome, $descrizione, $username, $data, $trainer);
-        $idSchedaQuery = $connessione->doReadQuery("SELECT max(id) as maxID FROM allenamento WHERE username_utente = ?","s",$username);
-        $idScheda= $idSchedaQuery[0]["maxID"];
-        $connessione->closeConnection();
-        header("location: modificaAllenamento.php?id=".$idScheda);
+    } elseif (isset($_POST['eliminaEsercizioSubmit'])) {
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
+            $connessione = new DBAccess();
+            $connessioneOK = $connessione->openDBConnection();
+
+            if ($connessioneOK) {
+                $isAdminQuery = $connessione->doReadQuery("SELECT isAdmin from utente where username=?","s",$_SESSION["username"]);
+                $schedaQuery = $connessione->doReadQuery("SELECT * from allenamento where id=?", "i", $_GET["id"]);
+                if(($schedaQuery != null) && ($isAdminQuery[0]["isAdmin"] == true || $schedaQuery[0]["username_utente"] == $_SESSION["username"])){
+                    $idScheda = $_GET["id"];
+                    $nomeEsercizio = $_POST["esercizioScheda"];
+                    $connessione->doWriteQuery("DELETE FROM allenamento_esercizio WHERE id_allenamento = ? AND nome_esercizio = ?","is",$idScheda,$nomeEsercizio);
+                    $connessione->closeConnection();
+                    header("location: modificaAllenamento.php?id=".$_GET["id"]);
+                } else {
+                    echo "non puoi accedere a questa pagina";
+                }  
+            } else {
+                $out = "<p>I sistemi sono al momento non disponibili, riprova più tardi!</p>";
+            }
         } else {
-            $out = "<p>I sistemi sono al momento non disponibili, riprova più tardi!</p>";
+            header("location: autenticaione.php");
         }
-    } else {
-        header("location: autenticaione.php");
+        
+    } elseif (isset($_POST['aggiungiEsercizioSubmit'])){
+        if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
+            $connessione = new DBAccess();
+            $connessioneOK = $connessione->openDBConnection();
+
+            if ($connessioneOK) {
+                $isAdminQuery = $connessione->doReadQuery("SELECT isAdmin from utente where username=?","s",$_SESSION["username"]);
+                $schedaQuery = $connessione->doReadQuery("SELECT * from allenamento where id=?", "i", $_GET["id"]);
+                if(($schedaQuery != null) && ($isAdminQuery[0]["isAdmin"] == true || $schedaQuery[0]["username_utente"] == $_SESSION["username"])){
+                    $idScheda = $_GET["id"];
+                    $nomeEsercizio = $_POST["nomeEsercizio"];
+                    $peso = $_POST["pesoEsercizio"] == 0 ? null : $_POST["pesoEsercizio"];
+                    $ripetizioni = $_POST["ripetizioniEsercizio"] == 0 ? null : $_POST["ripetizioniEsercizio"];
+                    $serie = $_POST["serieEsercizio"] == 0 ? null : $_POST["serieEsercizio"];
+                    $durata = $_POST["durataEsercizio"] == 0 ? null : $_POST["durataEsercizio"];
+
+                    $esercizioQuery = $connessione->doReadQuery("SELECT nome from esercizio where nome=?","s",$nomeEsercizio);
+                    if($esercizioQuery == null){
+                        $connessione->doWriteQuery("INSERT into esercizio(nome) values(?)","s",$nomeEsercizio);
+                    }
+                    $connessione->doWriteQuery("INSERT INTO allenamento_esercizio(id_allenamento,nome_esercizio,peso,ripetizioni,serie,durata) values(?,?,?,?,?,?)","isdiid",$idScheda,$nomeEsercizio,$peso,$ripetizioni,$serie,$durata);
+
+                    $connessione->closeConnection();
+                    header("location: modificaAllenamento.php?id=".$_GET["id"]);
+                } else {
+                    echo "non puoi accedere a questa pagina";
+                }  
+            } else {
+                $out = "<p>I sistemi sono al momento non disponibili, riprova più tardi!</p>";
+            }
+        } else {
+            header("location: autenticaione.php");
+        }
     }
 ?>
--- modificaAllenamento.php
+++ modificaAllenamento.php
@@ -41,20 +41,46 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
 
         $connessione->closeConnection();
         if(($schedaQuery != null) && ($isAdminQuery[0]["isAdmin"] == true || $schedaQuery[0]["username_utente"] == $_SESSION["username"])){
-            $optionEsercizio = "";
+            //form aggiungi esercizio
+            $aggiungiEsercizio = 
+            "<form id=\"aggiungiEsercizioForm\" action=\"generatoreScheda.php?id=". $_GET["id"] ."\", method=\"post\">
+                <label>Nome</label>
+                <input type=\"text\" name=\"nomeEsercizio\" required>
+                
+                <label>Peso</label>
+                <input type=\"number\" name=\"pesoEsercizio\" min=\"0\" value=\"0\" step=\"0.1\">
+                
+                <label>Serie</label>
+                <input type=\"number\" name=\"serieEsercizio\" min=\"0\" value=\"0\">
+                
+                <label>Ripetizioni</label>
+                <input type=\"number\" name=\"ripetizioniEsercizio\" min=\"0\" value=\"0\">
+
+                <label>Durata</label>
+                <input type=\"number\" name=\"durataEsercizio\" min=\"0\", value=\"0\", step=\"0.01\">
+                
+                <input type=\"submit\" value=\"Aggiungi\" name=\"aggiungiEsercizioSubmit\">
+            </form>";
+            $paginaHTML = str_replace("<insertEsercizio/>",$aggiungiEsercizio,$paginaHTML);
+
+            //form elimina esercizio
+            $optionEsercizio = "<form id=\"eliminaEsercizioForm\" action=\"generatoreScheda.php?id=".$_GET["id"]."\", method=\"post\"><select name=\"esercizioScheda\"><label>Seleziona esercizio</label>";
             foreach($eserciziQuery as $row){
                 $optionEsercizio .= "<option value=\"" . $row["nome_esercizio"] . "\">" . $row["nome_esercizio"] . "</option>";
             }
-            $paginaHTML = str_replace("<selectEsercizio/>",$optionEsercizio,$paginaHTML);
+            $optionEsercizio .= "<input type=\"submit\" value=\"Elimina\" name=\"eliminaEsercizioSubmit\"></select></form>";
+            $paginaHTML = str_replace("<deleteEsercizio/>",$optionEsercizio,$paginaHTML);
             
+            //dati scheda
             $datiScheda = "<div id=\"abbonamenti\">";
             $datiScheda .= "<h2>" . $schedaQuery[0]["nome"] . "</h2>"; 
             $datiScheda .= "<p>Descrizione: " . $schedaQuery[0]["descrizione"] ."</p>";
             $datiScheda .= "<p>Data creazione: " . $schedaQuery[0]["data_creazione"] . "</p>";
             
             foreach($eserciziQuery as $row){
+                //TODO: non mostrare serie, durata ecc se valore è null
                 $datiScheda .= "<div class=\"esercizio\"><h2>" . $row["nome_esercizio"] ."</h2><ul><li>peso:" . 
-                $row["peso"] . "</li><li>Serie: ". $row["serie"] ."</li><li>ripetizioni". $row["ripetizioni"] ."</li></ul></div>";
+                $row["peso"] . "</li><li>Serie: ". $row["serie"] ."</li><li>ripetizioni: ". $row["ripetizioni"] ."</li><li>durata: ". $row["durata"] ."</li></ul></div>";
             }
             
             $datiScheda .= "</div>";
