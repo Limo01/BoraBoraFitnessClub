cbce2c2b7258318f5f39ca43fbdcedd8d2343e92
Limo01 <marcoandrea0145@gmail.com>
2022-01-26 00:13:40 +0100

    Burger Menu

---
+++ burger-menu/mobile-js.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("topbar").style.width= "100%";
+  document.getElementById("burger-menu").style.display= "none";
+}
+
+function closeNav() {
+  document.getElementById("topbar").style.width = "0%";
+  document.getElementById("burger-menu").style.display= "block";
+} 
