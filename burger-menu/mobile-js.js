cdb84461cddc5fbae424dd001f4cc2cfc784c7de
Limo01 <marcoandrea0145@gmail.com>
2022-01-26 11:32:25 +0100

    Fixato bug sul ridimensionamento

--- burger-menu/mobile-js.js
+++ burger-menu/mobile-js.js
@@ -1,9 +1,31 @@
 function openNav() {
-  document.getElementById("topbar").style.width= "100%";
-  document.getElementById("burger-menu").style.display= "none";
+	var topbar= document.getElementById("topbar");
+	var burger_menu= document.getElementById("burger-menu");
+
+	topbar.style.width= "100%";
+	burger_menu.style.display= "none";
+
+	window.addEventListener("resize", function(){	
+		if(window.innerWidth > 600){
+			topbar.style.width= "100%";
+			burger_menu.style.display= "none";
+		}
+		else{
+			topbar.style.width= "0%";
+			burger_menu.style.display= "block";
+		}
+	});
 }
 
 function closeNav() {
-  document.getElementById("topbar").style.width = "0%";
-  document.getElementById("burger-menu").style.display= "block";
-} 
+	var topbar= document.getElementById("topbar");
+	var burger_menu= document.getElementById("burger-menu");
+
+	topbar.style.width = "0%";
+
+	topbar.addEventListener("transitionend", function(){
+		if(topbar.style.width == "0%"){
+			burger_menu.style.display= "block";
+		}
+	});
+}
